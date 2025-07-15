9764b79b141735c4d8f28aae40a782f70deb9375
Art <artyhoo@icloud.com>
2025-07-15 13:38:06 +0300

    npm dependency

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { useDarkMode } from "../context/DarkModeContext";
+import useDarkMode from "../context/useDarkMode";
 // import tw from "tailwind-styled-components";
 
 // // const StyledLogo = tw.div`
